c1094f57951deaa07f631ca137bcd73f057269f6
Jeremy <aviathur@gmail.com>
2022-06-24 18:12:47 -0400

    menu

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Pizza
 
-# Create your views here.
+
+def index(request):
+   ''' pizzas = Pizza.objects.all()
+    pizza_names_prices = [pizza.nom + " : " + str(pizza.prix) + "€"for pizza in pizzas]
+    pizza_names_prices_str = ", ".join(pizza_names_prices)
+    return HttpResponse("Les pizzas :" + pizza_names_prices_str)'''
+   pizzas = Pizza.objects.all().order_by("prix")
+   return render(request, 'menu/index.html', {'pizzas': pizzas})
